1fd4fe1ce8258405d92ce765adb88c0b2499f77b
TANGUY Arnaud <arn.tanguy@gmail.com>
2011-12-01 18:44:06 +0100

    Basic web view

--- NuitInfoSkyteem/src/skyteem/nuitinfo/NuitInfoSkyteemActivity.java
+++ NuitInfoSkyteem/src/skyteem/nuitinfo/NuitInfoSkyteemActivity.java
@@ -2,12 +2,21 @@ package skyteem.nuitinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class NuitInfoSkyteemActivity extends Activity {
+	WebView webView=null;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        webView = (WebView) findViewById(R.id.webView);
+        // Simplest usage: note that an exception will NOT be thrown
+        // if there is an error loading this page (see below).
+        webView.loadUrl("http://slashdot.org/");
+    
     }
 }
