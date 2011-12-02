70fe819f5a31e4b596fc02980b6f670a34220ce5
TANGUY Arnaud <arn.tanguy@gmail.com>
2011-12-02 01:15:58 +0100

    Change url

--- NuitInfoSkyteem/src/skyteem/easygift/EasyGiftActivity.java
+++ NuitInfoSkyteem/src/skyteem/easygift/EasyGiftActivity.java
@@ -1,16 +1,14 @@
 package skyteem.easygift;
 
 import android.os.Bundle;
-import android.webkit.WebView;
 import com.phonegap.DroidGap;
 
 public class EasyGiftActivity extends DroidGap {
-	WebView webView=null;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("http://easygift.druil.net/test.html");
+        super.loadUrl("http://easygift.druil.net");
     }
 }
